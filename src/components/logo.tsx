312af80c7a86f3ea8d521f0b0994dad868f11190
shkarDiary <95171810+shkarDiary@users.noreply.github.com>
2024-07-15 20:42:01 +0300

    continue on navbar

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,7 +1,8 @@
 import { Alegreya } from "@/app/fonts";
+import { cn } from "@/lib/utils";
 
 function Logo() {
-  return <h1 className={`${Alegreya.className}`}>SHKAR</h1>;
+  return <h1 className={cn("text-2xl", Alegreya.className)}>SHKAR</h1>;
 }
 
 export default Logo;
--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -1,24 +1,66 @@
 "use client";
 import { noto } from "@/app/fonts";
 import Logo from "./logo";
-import NavBtn from "./nav-btn";
 import { useState } from "react";
-import { motion } from "framer-motion";
+import { motion, useAnimate } from "framer-motion";
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+
+const navItems = [
+  "هەواڵ",
+  "راپۆرت",
+  "شیکردنەوە",
+  "بیروڕا",
+  "بەرنامەکان",
+  "گەلەری",
+  "وەرزش",
+  "ئێن ئاڕ تی دوو",
+];
 
 function Navigation() {
   const [isOpen, setIsOpen] = useState(false);
+  const [scope, animate] = useAnimate();
+
+  const onNavBtnClick = () => {
+    setIsOpen(!isOpen);
+    if (isOpen) {
+      animate([[".top", { rotate: 0, y: 0 }, { duration: 0.5 }]]);
+      animate([[".middle", { opacity: 100 }, { duration: 0.2 }]]);
+      animate([[".bottom", { rotate: 0, y: 0 }, { duration: 0.5 }]]);
+      animate([
+        [".container", { opacity: 0, display: "none" }, { duration: 0.6 }],
+      ]);
+    } else if (!isOpen) {
+      animate([[".top", { rotate: 60, y: 6.5 }, { duration: 0.5 }]]);
+      animate([[".middle", { opacity: 0 }, { duration: 0.2 }]]);
+      animate([[".bottom", { rotate: -60, y: -6.5 }, { duration: 0.5 }]]);
+      animate([
+        [".container", { opacity: 100, display: "block" }, { duration: 0.6 }],
+      ]);
+    }
+  };
+
   return (
-    <nav className="flex flex-col gap-6 px-8 py-4 ">
+    <nav ref={scope} className="flex flex-col gap-6 px-8 py-4 ">
       <div className="flex justify-between">
         <Logo />
-        <NavBtn onClick={() => setIsOpen(!isOpen)} />
+        <button type="button" title="menu" onClick={onNavBtnClick}>
+          <div className="top h-[0.18rem] w-6 bg-black  " />
+          <div className="middle my-1 h-[0.18rem] w-6 bg-black " />
+          <div className="bottom h-[0.18rem] w-6 bg-black " />
+        </button>
       </div>
       <ul
-        className={`${noto.className} ${isOpen ? "block" : "hidden"}  space-y-4 text-center text-xl md:flex`}
+        className={cn(
+          `container hidden space-y-6 text-center text-2xl md:flex`,
+          noto.className,
+        )}
       >
-        <li>سیاسی</li>
-        <li>کۆمەڵایەتی</li>
-        <li>وتار</li>
+        {navItems.map((item, index) => (
+          <Link className="block" href={""} key={index}>
+            {item}
+          </Link>
+        ))}
       </ul>
     </nav>
   );
